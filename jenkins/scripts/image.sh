b5346077e88465e5dfacd467f8551a8fdadcba7e
陈子昂 <121872494+Michael-Chen-NJU@users.noreply.github.com>
2024-08-16 00:39:21 +0800

    Update image.sh

--- jenkins/scripts/image.sh
+++ jenkins/scripts/image.sh
@@ -4,10 +4,10 @@ set -x
 
 VERSION_ID="${BUILD_ID}"
 IMAGE_NAME="cloud-native-project:${BUILD_ID}"
-IMAGE_ADDR="test-harbor.daocloud.io/cn202006/${IMAGE_NAME}"
+IMAGE_ADDR="test-harbor.daocloud.io/nju18/${IMAGE_NAME}"
  
 docker build -f Dockerfile --build-arg jar_name=target/prometheus-test-demo-0.0.1-SNAPSHOT.jar -t ${IMAGE_NAME}:${VERSION_ID} .
  
 docker tag  ${IMAGE_NAME}:${VERSION_ID}  ${IMAGE_ADDR}:${VERSION_ID}
-docker login --username=cn202006 --password=cn202006 test-harbor.daocloud.io
+docker login --username=nju18 --password=cloudnju18 test-harbor.daocloud.io
 docker push ${IMAGE_ADDR}:${VERSION_ID}
